717a2d35278057e8834eba451c7ef3b14021d742
Roberto Alvira <alvira5@hotmail.com>
2023-11-18 16:53:15 -0500

    continuar con btn y weathers elem svgs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=
 const searchBox = document.querySelector (".weather-search input ");
 const searchBtn = document.querySelector (".weather-search button");
 
-const fToC = document.querySelector(".clickFToC button");
+var fToC = document.querySelector(".clickFToC");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -20,12 +20,36 @@ async function checkWeather(city){
 }
 
 
-
+// Le dan click al icono
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 
 })
 
 
+// Apretar enter
+ searchBox.addEventListener('keydown',(event)=> {
+    if(event.key ==='Enter'){
+        checkWeather(searchBox.value)
+    }
 
- 
+ })
+
+
+//  Cambiar metrica
+
+function toggleText(fToC){
+    var texto = fToC.textContent || fToC.innerText;
+    if(texto == "Celcius | kph"){
+        fToC.innerTextL = 'Farenheit | mph'
+    } else {
+        fToC.innerText = "Celcius | kph";
+    }
+}
+
+
+// function changeText(event){
+//    event.innerHTML = "Farenheit | mph";
+    
+// }onclick="changeText(this)"
+
